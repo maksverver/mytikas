4d2ebd7493fd92f842902e2d2e666446316854ab
Maks Verver <maks@verver.ch>
2025-08-31 19:45:16 +0200

    Make build script compatible with Github Actions.

--- build-wasm-api.sh
+++ build-wasm-api.sh
@@ -1,31 +1,33 @@
 #!/bin/sh
 #
-# Builds the Javascript WASM API. Requires Emscripten to be installed!
+# Builds the Javascript WASM API. Requires emcmake to be installed in
+# /usr/lib/emscripten or in another directory that is added to PATH.
 #
 # This generates wasm-api.wasm and wasm-api.js, and copies the files to the www/
 # subdirectory.
 
-set -e -E -o pipefail
+set -e
 
 cd "$(dirname "$(readlink -f "$0")")"
 
 BUILD_DIR=build-wasm-api
 INSTALL_DIR=www/generated
-TOOLCHAIN=/usr/lib/emscripten/cmake/Modules/Platform/Emscripten.cmake
-NODE=/usr/bin/node
+PATH=/usr/lib/emscripten:${PATH}
 
 if ! [ -d "$BUILD_DIR" ]; then
-    # Equivalent to:
-    #
-    #/usr/lib/emscripten/emcmake cmake -B "$BUILD_DIR" -D CMAKE_BUILD_TYPE=Release
-    #
-    cmake \
-           -B "$BUILD_DIR" \
-           -D CMAKE_BUILD_TYPE=Release \
-           -D CMAKE_TOOLCHAIN_FILE="$TOOLCHAIN" \
-           -D CMAKE_CROSSCOMPILING_EMULATOR="$NODE"
+    emcmake cmake -B "$BUILD_DIR" -D CMAKE_BUILD_TYPE=Release
+
+    # Should be equivalent to:
+    #cmake \
+    #       -B "$BUILD_DIR" \
+    #       -D CMAKE_BUILD_TYPE=Release \
+    #       -D CMAKE_TOOLCHAIN_FILE=/usr/lib/emscripten/cmake/Modules/Platform/Emscripten.cmake \
+    #       -D CMAKE_CROSSCOMPILING_EMULATOR=/usr/bin/node
 fi
 
 cmake --build "$BUILD_DIR" --target wasm-api
 
-cp -f -l "$BUILD_DIR"/apps/wasm-api.{js,wasm} www/generated/
+cp -f -l \
+    "$BUILD_DIR"/apps/wasm-api.js \
+    "$BUILD_DIR"/apps/wasm-api.wasm \
+    www/generated/
